a94a52d20236bb9d73a220036607ddd71dd29a88
Alexandre Dubray <alexandre.dubray@uclouvain.be>
2022-06-09 11:19:16 +0200

    add license headers

--- src/core/graph.rs
+++ src/core/graph.rs
@@ -1,3 +1,19 @@
+//Schlandals
+//Copyright (C) 2022 A. Dubray
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU Affero General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU Affero General Public License for more details.
+//
+//You should have received a copy of the GNU Affero General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 //! This module implements a graph using vector indexes. This implementation is inspired by [this
 //! blog post](https://smallcultfollowing.com/babysteps/blog/2015/04/06/modeling-graphs-in-rust-using-vector-indices/)
 //! and [this
