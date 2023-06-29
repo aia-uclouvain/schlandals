0fc22bebf46e5a77a1871746db198d5aa7ea3daf
Alexandre Dubray <alexandre.dubray@uclouvain.be>
2023-06-29 10:08:06 +0200

    [no bench] Use subcommands for CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ mod compiler;
 mod heuristics;
 mod propagator;
 
-use clap::{Parser, ValueEnum};
+use clap::{Parser, ValueEnum, Subcommand};
 use std::path::PathBuf;
 use std::fs::File;
 use std::io::Write;
@@ -37,35 +37,55 @@ use propagator::FTReachablePropagator;
 use compiler::exact::ExactDACCompiler;
 use compiler::circuit::DAC;
 
-#[derive(Parser)]
-#[clap(author, version, about, long_about = None)]
-struct Args {
-    #[clap(short, long, value_parser)]
-    input: PathBuf,
-    // How to branch
-    #[clap(short, long, value_enum)]
-    branching: Branching,
-    // Collect stats during the search, default yes
-    #[clap(short, long, action)]
-    statistics: bool,
-    // The memory limit, in mega-bytes
-    #[clap(short, long)]
-    memory: Option<u64>,
-    // If given, the problem is compiled into a probabilistic circuit and the memory limit is not taken into account
-    #[clap(short, long, action)]
-    compiled: bool,
-    // In which file to store/read the compiled probabilistic circuit
-    #[clap(long)]
-    fpc: Option<PathBuf>,
-    #[clap(short, long, action)]
-    // If present, the compiler reads from the given file and evaluate the circuits
-    read_compiled: bool,
-    // Store a DOT representation of the compiled circuit
-    #[clap(long)]
-    dotfile: Option<PathBuf>,
+#[derive(Debug, Parser)]
+#[clap(name="Schlandals", version, author, about)]
+pub struct App {
+    #[clap(subcommand)]
+    command: Command,
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Search based solver
+    Search {
+        /// The input file
+        #[clap(short, long, value_parser)]
+        input: PathBuf,
+        // How to branch
+        #[clap(short, long, value_enum)]
+        branching: Branching,
+        // Collect stats during the search, default yes
+        #[clap(short, long, action)]
+        statistics: bool,
+        // The memory limit, in mega-bytes
+        #[clap(short, long)]
+        memory: Option<u64>,
+    },
+    Compile {
+        /// The input file
+        #[clap(short, long, value_parser)]
+        input: PathBuf,
+        // How to branch
+        #[clap(short, long, value_enum)]
+        branching: Branching,
+        /// If present, store a textual representation of the compiled circuit
+        #[clap(long)]
+        fdac: Option<PathBuf>,
+        // If present, store a DOT representation of the compiled circuit
+        #[clap(long)]
+        dotfile: Option<PathBuf>,
+    },
+    ReadCompiled {
+        /// Reads a circuit from an input file
+        #[clap(short, long, value_parser)]
+        input: PathBuf,
+        // If present, store a DOT representation of the compiled circuit
+        #[clap(long)]
+        dotfile: Option<PathBuf>,
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
 enum Branching {
     /// Heuristic based on the fiedler value of the clause graph
     Fiedler,
@@ -77,68 +97,75 @@ enum Branching {
     MaxDegree
 }
 
-fn run_compilation(args: &Args) {
-    if args.read_compiled {
-        let mut spn = DAC::from_file(args.fpc.as_ref().unwrap());
-        println!("{:?}", spn.evaluate());
-    } else {
-        let mut state = StateManager::default();
-        let mut propagator = FTReachablePropagator::<true>::new();
-        let graph = graph_from_ppidimacs(&args.input, &mut state, &mut propagator);
-        let component_extractor = ComponentExtractor::new(&graph, &mut state);
-        let mut branching_heuristic: Box<dyn BranchingDecision> = match args.branching {
-            Branching::Fiedler => Box::<Fiedler>::default(),
-            Branching::MinInDegree => Box::<MinInDegree>::default(),
-            Branching::MinOutDegree => Box::<MinOutDegree>::default(),
-            Branching::MaxDegree => Box::<MaxDegree>::default(),
-        };
-        let mut compiler = ExactDACCompiler::new(graph, state, component_extractor, branching_heuristic.as_mut(), propagator);
-        match compiler.compile() {
-            None => {
-                println!("Model UNSAT, can not compile");       
-            },
-            Some(pc) => {
-                println!("Compilation successfull");
-                if args.dotfile.is_some() {
-                    let out = pc.as_graphviz();
-                    let mut outfile = File::create(args.dotfile.as_ref().unwrap()).unwrap();
-                    match outfile.write(out.as_bytes()) {
-                        Ok(_) => (),
-                        Err(e) => println!("Culd not write the PC into the file: {:?}", e),
-                    }
-                }
-                if args.fpc.is_some() {
-                    let mut outfile = File::create(args.fpc.as_ref().unwrap()).unwrap();
-                    match outfile.write(format!("{}", pc).as_bytes()) {
-                        Ok(_) => (),
-                        Err(e) => println!("Culd not write the PC into the file: {:?}", e),
-                    }
-                    
-                }
-            }
+fn read_compiled(input: PathBuf, dotfile: Option<PathBuf>) {
+    let mut dac = DAC::from_file(&input);
+    if let Some(f) = dotfile {
+        let out = dac.as_graphviz();
+        let mut outfile = File::create(&f).unwrap();
+        match outfile.write(out.as_bytes()) {
+            Ok(_) => (),
+            Err(e) => println!("Culd not write the PC into the file: {:?}", e),
         }
-
     }
+    println!("{}", dac.evaluate());
 }
 
-fn run_search(args: &Args) {
+fn run_compilation(input: PathBuf, branching: Branching, fdac: Option<PathBuf>, dotfile: Option<PathBuf>) {
     let mut state = StateManager::default();
-    let mut propagator = FTReachablePropagator::<false>::new();
-    let graph = graph_from_ppidimacs(&args.input, &mut state, &mut propagator);
+    let mut propagator = FTReachablePropagator::<true>::new();
+    let graph = graph_from_ppidimacs(&input, &mut state, &mut propagator);
     let component_extractor = ComponentExtractor::new(&graph, &mut state);
-    let mut branching_heuristic: Box<dyn BranchingDecision> = match args.branching {
+    let mut branching_heuristic: Box<dyn BranchingDecision> = match branching {
         Branching::Fiedler => Box::<Fiedler>::default(),
         Branching::MinInDegree => Box::<MinInDegree>::default(),
         Branching::MinOutDegree => Box::<MinOutDegree>::default(),
         Branching::MaxDegree => Box::<MaxDegree>::default(),
     };
-    let mlimit = if args.memory.is_some() {
-        args.memory.unwrap()
+    let mut compiler = ExactDACCompiler::new(graph, state, component_extractor, branching_heuristic.as_mut(), propagator);
+    match compiler.compile() {
+        None => {
+            println!("Model UNSAT, can not compile");       
+        },
+        Some(dac) => {
+            println!("Compilation successfull");
+            if let Some(f) = dotfile {
+                let out = dac.as_graphviz();
+                let mut outfile = File::create(&f).unwrap();
+                match outfile.write(out.as_bytes()) {
+                    Ok(_) => (),
+                    Err(e) => println!("Culd not write the PC into the file: {:?}", e),
+                }
+            }
+            if let Some(f) = fdac {
+                let mut outfile = File::create(&f).unwrap();
+                match outfile.write(format!("{}", dac).as_bytes()) {
+                    Ok(_) => (),
+                    Err(e) => println!("Culd not write the PC into the file: {:?}", e),
+                }
+                
+            }
+        }
+    }
+}
+
+fn run_search(input: PathBuf, branching: Branching, statistics: bool, memory: Option<u64>) {
+    let mut state = StateManager::default();
+    let mut propagator = FTReachablePropagator::<false>::new();
+    let graph = graph_from_ppidimacs(&input, &mut state, &mut propagator);
+    let component_extractor = ComponentExtractor::new(&graph, &mut state);
+    let mut branching_heuristic: Box<dyn BranchingDecision> = match branching {
+        Branching::Fiedler => Box::<Fiedler>::default(),
+        Branching::MinInDegree => Box::<MinInDegree>::default(),
+        Branching::MinOutDegree => Box::<MinOutDegree>::default(),
+        Branching::MaxDegree => Box::<MaxDegree>::default(),
+    };
+    let mlimit = if memory.is_some() {
+        memory.unwrap()
     } else {
         let sys = System::new_all();
         sys.total_memory() / 1000000
     };
-    if args.statistics {
+    if statistics {
         let mut solver = DefaultSolver::new(
             graph,
             state,
@@ -162,10 +189,23 @@ fn run_search(args: &Args) {
 }
 
 fn main() {
-    let args = Args::parse();
+    let app = App::parse();
+    match app.command {
+        Command::Search { input, branching, statistics, memory } => {
+            run_search(input, branching, statistics, memory);
+        },
+        Command::Compile { input, branching, fdac, dotfile } => {
+            run_compilation(input, branching, fdac, dotfile);
+        }
+        Command::ReadCompiled { input, dotfile } => {
+            read_compiled(input, dotfile);
+        }
+    }
+    /*
     if args.compiled {
         run_compilation(&args);
     } else {
         run_search(&args);
     }
+    */
 }
